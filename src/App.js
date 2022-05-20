060887671c50eb41c9ec067f2cf2da5b3f04dba2
Humaira Rahaman <humairamitul@gmail.com>
2022-05-21 03:26:00 +0600

    animation added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Choose from './components/Choose/Choose'
 import Home from './components/Home/Home'
@@ -7,6 +9,10 @@ import Navbar from './components/Navbar/Navbar'
 
 
 const App = () => {
+
+  useEffect(()=>{
+    AOS.init();
+  },[])
   return (
     <div className=" max-w-7xl mx-auto px-12">
       <Navbar></Navbar>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div className=" justify-center flex py-2">
+    <div className=" bg-gradient-to-r from-indigo-100 via-cyan-50 to-blue-100  justify-center flex mb-10 pt-1  shadow-xl rounded-2xl shadow-cyan-200">
       <ul className="menu menu-horizontal p-0 flex justify-evenly w-full ">
         <li className="text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-indigo-600   mb-4 cursor-pointe font-bold">
           <NavLink to="/">Home</NavLink>
